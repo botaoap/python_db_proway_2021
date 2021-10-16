f4b724fa18c39015a76e818425ec6583bae39124
lordefps <gabrielbotaoap@gmail.com>
2021-10-16 18:15:55 -0300

    Aula 5 anotations DB

---
+++ aula5/db/aula1DB.py
@@ -0,0 +1,24 @@
+# SGDB - Sistema Gerenciador de Bancos de Dados
+
+# Analogia
+# um SGDB podemos comparar com uma IDE que utilizamos para formatar ou criar nosso codigos
+# Como por exemplo o VsCode - PyCharm - Emac
+
+# Um SGDB entao serve para gerenciarmos um banco de dados podendo:
+#
+# - CRIAR TABELAS
+# - CRIAR BASE DE DADOS
+# - ALTERAR COLUNAS
+# - ALTERAR LINHAS INTEIRAS
+# - RESGATAR AS INFORMACOES
+# - DELETAR
+# - INSERIR DADOS
+
+# E muito mais finalidades
+
+# Esemplo de SGDBs
+# MYSQL - POSTEGRESS - SQL SERVER - AURORA - ORACLE - SQLITE
+
+# BANCO DE DADOS
+
+# Neste curso utilizaremos o MySQL para aprendizado de alguns conceitos e comandos
